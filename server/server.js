8fb8ffabbcdc54320a661f77b37fdc23d04db8ae
iozeen <iozintv@gmail.com>
2017-09-06 17:58:34 +0300

    Setuo app for heroku

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+const { ObjectID } = require('mongodb');
 var { mongoose } = require('./db/mongoos');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -30,8 +31,26 @@ app.get('/todos', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send('Not valid id');
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({ todo });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
+
+});
+
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = { app };
